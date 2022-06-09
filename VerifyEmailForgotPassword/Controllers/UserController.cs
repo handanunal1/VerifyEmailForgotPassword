79158d509d9e60c6317de0b1975e607185bbeb9f
handan unal <handan.unal@yahoo.com>
2022-06-09 21:56:08 +0300

    Verification

--- VerifyEmailForgotPassword/Controllers/UserController.cs
+++ VerifyEmailForgotPassword/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 namespace VerifyEmailForgotPassword.Controllers
 {
@@ -7,6 +8,93 @@ namespace VerifyEmailForgotPassword.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+
+        private readonly DataContext _dataContext;
+        public UserController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if (_dataContext.Users.Any(u => u.EmailAdress == request.Email))
+            {
+                return BadRequest("User already exists");
+            }
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            var user = new User
+            {
+                EmailAdress = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+
+            _dataContext.Users.Add(user);
+            await _dataContext.SaveChangesAsync();
+            return Ok("User successfully created");
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginRequest request)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.EmailAdress == request.Email);
+            if (user == null)
+            {
+                return BadRequest("User not found!");
+            }
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Password is incorrect");
+            }
+            if (user.VerifiedAt == null)
+            {
+                return BadRequest("Not verified!");
+            }
+            return Ok($"Welcome back, {user.EmailAdress}");
+
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+
+
+
+        [HttpPost("verify")]
+        public async Task<IActionResult> Verify(string token)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.VerificationToken == token);
+            if(user == null)
+            {
+                return BadRequest("Not found");
+            }
+
+            user.VerifiedAt = DateTime.Now;
+            await _dataContext.SaveChangesAsync();
+  
+            return Ok("User verified");
+
+        }
 
 
     }
--- VerifyEmailForgotPassword/Models/UserRegisterRequest.cs
+++ VerifyEmailForgotPassword/Models/UserRegisterRequest.cs
@@ -6,7 +6,7 @@ namespace VerifyEmailForgotPassword.Models
     {
         [Required , EmailAddress]
         public string Email { get; set; } = string.Empty;
-        [Required, MinLength(6)]
+        [Required, MinLength(6,ErrorMessage =" Please enter at least 6 characters! ")]
         public string Password { get; set; } = string.Empty;
         [Required, Compare("Password")]
         public string ConfirmPassword { get; set; } = string.Empty;
