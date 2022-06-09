57dce3bbb96ecf8980180abcf34212685ebc92ba
handan unal <handan.unal@yahoo.com>
2022-06-09 21:10:01 +0300

    Add project files.

---
+++ VerifyEmailForgotPassword/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace VerifyEmailForgotPassword.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string EmailAdress { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+        public string? VerificationToken { get; set; } 
+        public DateTime? VerifiedAt { get; set; }
+        public string? PasswordResetToken { get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
+
+
+    }
+}
